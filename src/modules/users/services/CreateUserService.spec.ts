8565713794142fc94a63feb24bbdb23e1ac553e4
John Freitas <john.freitasau@gmail.com>
2020-06-16 23:54:16 +1000

    Create user - Unit test completed

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,11 +1,9 @@
-// import AppError from '@shared/errors/AppError';
-// import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
-// import CreateAppointmentService from './CreateAppointmentService';
+import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import CreateUserService from '@modules/users/services/CreateUserService';
 
 describe('CreateUser', () => {
-  it('Should be able to create a user', async () => {
+  it('Should be able to create a user.', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
     const createUserService = new CreateUserService(fakeUsersRepository);
 
@@ -16,5 +14,24 @@ describe('CreateUser', () => {
     });
 
     expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create the same user twice.', async () => {
+    const fakeUSersRepository = new FakeUsersRepository();
+    const createUserService = new CreateUserService(fakeUSersRepository);
+
+    await createUserService.execute({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      password: '12345',
+    });
+
+    expect(
+      createUserService.execute({
+        name: 'John Doe',
+        email: 'john.doe@gmail.com',
+        password: '12345',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
