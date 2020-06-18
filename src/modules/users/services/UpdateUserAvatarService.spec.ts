b60516dff9464d0599dadd9ea39fc1398590e225
John Freitas <john.freitasau@gmail.com>
2020-06-18 23:49:47 +1000

    Implemented USer avatar unit test

---
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -0,0 +1,76 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+// import CreateUserService from '@modules/users/services/CreateUserService';
+// import AppError from '@shared/errors/AppError';
+// import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
+// import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
+import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
+// import AuthenticateUserService from './AuthenticateUserService';
+// import FakeHashProvider from '../providers/fakes/FakeHashProvider';
+import AppError from '@shared/errors/AppError';
+
+import UpdateUserAvatarService from './UpdateUserAvatarService';
+
+describe('UpdateUserAvatar', () => {
+  it('It should be able to update the Avatar', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      password: '12345',
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.jpg', // it doesn't need to exist.
+    });
+
+    expect(user.avatar).toBe('avatar.jpg');
+  });
+
+  it('It should not allow non authenticated users to change Avatar', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    expect(
+      updateUserAvatar.execute({
+        user_id: 'non-existing-user',
+        avatarFilename: 'avatar.jpg', // it doesn't need to exist.
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('It should delete old avatar when updating a new one', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      password: '12345',
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.jpg', // it doesn't need to exist.
+    });
+
+    expect(user.avatar).toBe('avatar.jpg');
+  });
+});
