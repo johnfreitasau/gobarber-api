250747169d9be2cd473e739c43556bfd2180af7c
John Freitas <john.freitasau@gmail.com>
2020-06-18 22:40:31 +1000

    Implemented providers for Storage

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,5 +1,6 @@
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import CreateUserService from '@modules/users/services/CreateUserService';
+import AppError from '@shared/errors/AppError';
 import AuthenticateUserService from './AuthenticateUserService';
 import FakeHashProvider from '../providers/fakes/FakeHashProvider';
 
@@ -31,22 +32,46 @@ describe('AuthenticateUser', () => {
     expect(response.user).toEqual(user);
   });
 
-  it('shouldnt be able to authenticate using wrong email', async () => {
+  it('should not be able to authenticate using an non existing account.', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: '12345',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('It should not be able to authenticate using the wrong password', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
     const fakeHashProvider = new FakeHashProvider();
     const createUser = new CreateUserService(
       fakeUsersRepository,
       fakeHashProvider,
     );
-    // const authenticateUser = new AuthenticateUserService(
-    //   fakeUsersRepository,
-    //   fakeHashProvider,
-    // );
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     await createUser.execute({
       name: 'John Doe',
       email: 'john.doe@gmail.com',
       password: '12345',
     });
+
+    expect(
+      authenticateUser.execute({
+        email: 'john.doe@gmail.com',
+        password: 'wrong-password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
