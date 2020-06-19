47dbfe2057c51c973eb1bde74937e427c17d576e
John Freitas <john.freitasau@gmail.com>
2020-06-19 21:44:05 +1000

    Unit tests completed

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -55,6 +55,8 @@ describe('UpdateUserAvatar', () => {
     const fakeUsersRepository = new FakeUsersRepository();
     const fakeStorageProvider = new FakeStorageProvider();
 
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
     const updateUserAvatar = new UpdateUserAvatarService(
       fakeUsersRepository,
       fakeStorageProvider,
@@ -71,6 +73,12 @@ describe('UpdateUserAvatar', () => {
       avatarFilename: 'avatar.jpg', // it doesn't need to exist.
     });
 
-    expect(user.avatar).toBe('avatar.jpg');
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar2.jpg',
+    });
+
+    expect(deleteFile).toHaveBeenCalledWith('avatar.jpg');
+    expect(user.avatar).toBe('avatar2.jpg');
   });
 });
