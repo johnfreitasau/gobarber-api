2c2d75e293825eff12a0a4c8deddd53069fae737
John Freitas <john.freitasau@gmail.com>
2020-06-23 23:39:28 +1000

    MailProvider creation using Ethereal

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import appointmentRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/sessions', sessionsRouter);
 routes.use('/appointments', appointmentRouter);
 routes.use('/users', usersRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
