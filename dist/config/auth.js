36f1165e3a61eb9626493b856143e965c5f56694
John Freitas <john.freitasau@gmail.com>
2020-07-02 04:16:06 +1000

    .gitignore fix

--- dist/config/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwt: {
-        secret: 'd1b1e6eee8f39432caa7636dc8192fd2',
-        expiresIn: '1d',
-    },
-};
--- dist/modules/users/repositories/IUsersRepository.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// import CreateUserService from '../services/CreateUserService';
-Object.defineProperty(exports, "__esModule", { value: true });
