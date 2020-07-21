6798d240d376629ef4fb0c04da1c4cd63605b9a0
John Freitas <john.freitasau@gmail.com>
2020-07-22 08:00:33 +1000

    Adjusted LimitRater to fix FrontEnd issue

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { celebrate, Segments, Joi } from 'celebrate';
+// import { celebrate, Segments, Joi } from 'celebrate';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
 import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
@@ -25,9 +25,9 @@ providersRouter.get(
 
 providersRouter.get(
   '/:provider_id/day-availability',
-  celebrate({
-    [Segments.PARAMS]: Joi.string().uuid().required(),
-  }),
+  // celebrate({
+  //   [Segments.PARAMS]: Joi.string().uuid().required(),
+  // }),
   providerDayAvailabilityController.index,
 );
 
